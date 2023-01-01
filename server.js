4fc2d35c236a70776241e1ebbd4d8fa9b89fbc4d
kilik42 <maevins@cps.edu>
2022-12-31 20:15:47 -0600

    initial commit

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', require('./routes/root'))
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.all('*', (req, res) => { 
+    res.status(404).send('404 - Not Found');
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    };
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // Path: package.json
